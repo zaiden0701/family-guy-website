54f9de7edf8aa9ec75e7ba77552f278525647f95
Aiden Zafar <+>
2022-11-28 16:32:03 -0500

    initial JS for img render

--- script.js
+++ script.js
@@ -1,15 +1,36 @@
 //set
-const charImg = 'https://api.tvmaze.com/shows/84/cast'
+const mainImgUrl = 'https://api.tvmaze.com/shows/84/images'
+const charImgUrl = 'https://api.tvmaze.com/shows/84/cast'
+const div = document.querySelector('div')
 
-const h2 = document.createElement("h2");
-h2.textContent = "family test"
-document.querySelector("body").appendChild(h2);
+
+//initial image
+fetch(mainImgUrl)
+    .then((resp) => resp.json())
+    .then((json) => {
+        alert(JSON.stringify(json));
+    })
+
+function renderMainImg(main) {
+    main.forEach(mainImg => {
+      const img = document.createElement('img');
+      img.src = mainImg
+      img.innerHTML = mainImg.resolutions.medium
+      div.append(img);
+    });
+}
 
 // grab character images
-fetch(charImg)
-    .then((resp) => resp.json())
-    .then((charData) => renderCharImg(charData.message));
+// fetch(charImgUrl)
+//     .then((resp) => resp.json())
+//     .then((charData) => renderCharImg(charData.character.image.medium));
 
-function renderCharImg(char) {
-    
-}
+// function renderCharImg(char) {
+//     const div = document.querySelector('div');
+//     char.forEach(charImg => {
+//       const img = document.createElement('img');
+//       img.src = charImg;
+//       img.innerHTML = charImg.name;
+//       div.append(img);
+//     });
+// }
