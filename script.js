e0bc74c6f43483b47da03255e01e25024bf05e36
Aiden Zafar <+>
2022-11-28 14:34:03 -0500

    adding API for images

--- script.js
+++ script.js
@@ -1,4 +1,15 @@
+//set
+const charImg = 'https://api.tvmaze.com/shows/84/cast'
+
 const h2 = document.createElement("h2");
 h2.textContent = "family test"
-
 document.querySelector("body").appendChild(h2);
+
+// grab character images
+fetch(charImg)
+    .then((resp) => resp.json())
+    .then((charData) => renderCharImg(charData.message));
+
+function renderCharImg(char) {
+    
+}
