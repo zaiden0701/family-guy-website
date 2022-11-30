dfa000a89884a8337741f7b6c6c325cb5b265d6c
Aiden Zafar <+>
2022-11-29 21:25:57 -0500

    developing change event for dropdown menu

--- script.js
+++ script.js
@@ -20,17 +20,36 @@ const getCast = () => {
 getCast()
 
 //render main image
-// const renderMainImg = (main) => {
-
 function renderMainImg(main){
-    const div = document.querySelector('div');
+    const div1 = document.getElementById('img-container')
     main.forEach(mainImg => {
-        const img = document.createElement('img');
-        img.src = mainImg.resolutions.original.url;
-        img.innerHTML = mainImg.resolutions.original.url;
-        div.append(img);
-      });
+        const img = document.createElement('img')
+        img.src = mainImg.resolutions.original.url
+        img.innerHTML = mainImg.resolutions.original.url
+        div1.append(img)
+      })
 }
 
 //render character images
-const renderCastImg = (cast) => {}
+// function renderCastImg(cast){
+//     const div2 = document.getElementsByClassName('result')
+//     cast.forEach(charImg => {
+//         const img = document.createElement('img')
+//         img.src = charImg.character.image.medium
+//         img.innerHTML = charImg.character.image.medium
+//         div2.append(img)
+//       })
+// }
+
+//character card dropdown (change event)
+const select = document.querySelector('select')
+select.addEventListener('change', filterChar)
+
+// function filterChar(e) {
+//     let name = e.target.value
+//     let filteredChar = chars.filter((char) => {
+//         return char[0] === name
+//     })
+//     console.log(filteredChar)
+//     div.innerHTML = ''
+// }
